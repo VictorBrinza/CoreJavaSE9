4e50ef46711a6f4a6200d98e21a4ad60924bfb8f
Victor Brinza <brinza193md@gmail.com>
2018-04-30 17:49:29 +0300

    30/04/1018 commit

---
+++ JavaCoreSE9/src/ChapterIII/Exercises/Exercise1/Measurable.java
@@ -0,0 +1,9 @@
+
+package ChapterIII.Exercises.Exercise1;
+
+
+public interface Measurable {
+    double getMeasure();    
+    double average(Measurable[] obj);
+    public Object largest(Measurable[] list);
+}
